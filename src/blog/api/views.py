22e712fe970dc245359992c8e9c2cc27805711ee
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-01-29 14:15:16 -0300

    feat(blog): revised posts url, added fields to Post model

--- src/blog/api/views.py
+++ src/blog/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from .serializers import (
@@ -9,3 +10,14 @@ from ..models import Post
 class AllPostsView(ListAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    pagination_class = PageNumberPagination
+
+class PostDetailView(GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+
+    def get(self, request, slug):
+        post = self.get_object()
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
